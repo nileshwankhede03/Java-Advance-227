10e66a896e50fe6dd40767757894929ae147f9a4
Nilesh Wankhede <114302591+nileshwankhede03@users.noreply.github.com>
2025-09-17 15:30:58 +0530

    Add files via upload

---
+++ testDependancyInjection/src/main/java/com/wankhede/testDependancyInjection/Dev.java
@@ -0,0 +1,12 @@
+package com.wankhede.testDependancyInjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev
+{
+    public void Build()
+    {
+        System.out.println("working on awesome project...");
+    }
+}
---
+++ testDependancyInjection/src/test/java/com/wankhede/testDependancyInjection/TestDependancyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wankhede.testDependancyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDependancyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
